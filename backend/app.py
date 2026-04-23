be6ee965a7af9d67818934fe94073b24cb449bda
aiganysh <aiganysh.abdukaarova@alatoo.edu.kg>
2026-04-23 09:44:04 +0600

    added table

--- backend/app.py
+++ backend/app.py
@@ -6,7 +6,6 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-DATABASE_URL = os.getenv("DATABASE_URL")
 
 def get_conn():
     return psycopg2.connect(
@@ -14,41 +13,77 @@ def get_conn():
         sslmode="require"
     )
 
+
+def init_db():
+    conn = get_conn()
+    cur = conn.cursor()
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS items (
+            id SERIAL PRIMARY KEY,
+            name TEXT
+        )
+    """)
+    conn.commit()
+    cur.close()
+    conn.close()
+
+
+
 @app.route("/")
 def home():
     return "Backend is running!"
 
+
 @app.route("/api/data", methods=["GET"])
 def get_data():
+    init_db()
     conn = get_conn()
     cur = conn.cursor()
-    cur.execute("CREATE TABLE IF NOT EXISTS items (id SERIAL PRIMARY KEY, name TEXT)")
+
     cur.execute("SELECT * FROM items")
     rows = cur.fetchall()
+
     cur.close()
     conn.close()
+
     return jsonify(rows)
+
 
 @app.route("/api/data", methods=["POST"])
 def add_data():
+    init_db()
     data = request.json
+
     conn = get_conn()
     cur = conn.cursor()
-    cur.execute("INSERT INTO items (name) VALUES (%s)", (data["name"],))
+
+    cur.execute(
+        "INSERT INTO items (name) VALUES (%s)",
+        (data["name"],)
+    )
+
     conn.commit()
     cur.close()
     conn.close()
+
     return jsonify({"message": "added"})
+
 
 @app.route("/api/data/<int:id>", methods=["DELETE"])
 def delete_data(id):
     conn = get_conn()
     cur = conn.cursor()
+
     cur.execute("DELETE FROM items WHERE id=%s", (id,))
+
     conn.commit()
     cur.close()
     conn.close()
+
     return jsonify({"message": "deleted"})
 
+
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
